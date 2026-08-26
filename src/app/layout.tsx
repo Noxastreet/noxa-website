a06433b827627b5c0b434dc6ffda67151a7c2d65
poseidon <64107696+sergiosk222@users.noreply.github.com>
2026-08-26 09:38:01 +0300

    fix(viewport): enable safe-area viewport fitting

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,6 +43,7 @@ export const metadata: Metadata = {
 export const viewport: Viewport = {
   width: "device-width",
   initialScale: 1,
+  viewportFit: "cover",
   themeColor: "#050505",
   colorScheme: "dark",
 };
