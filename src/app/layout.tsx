cd13a4b351728832a7f3976c721063762cd1bccb
poseidon <64107696+sergiosk222@users.noreply.github.com>
2026-09-01 18:48:01 +0300

    feat: refresh NOXA site icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,8 +21,14 @@ export const metadata: Metadata = {
     },
   },
   icons: {
-    icon: [{ url: "/icon.svg", type: "image/svg+xml" }],
-    shortcut: "/icon.svg",
+    icon: [
+      { url: "/favicon.ico", sizes: "any" },
+      { url: "/icon.png", type: "image/png", sizes: "512x512" },
+    ],
+    shortcut: "/favicon.ico",
+    apple: [
+      { url: "/apple-icon.png", type: "image/png", sizes: "180x180" },
+    ],
   },
   robots: {
     index: true,
