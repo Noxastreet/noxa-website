80ac1d04bc6d47431a49a319cd2a6c1f537402d1
poseidon <64107696+sergiosk222@users.noreply.github.com>
2026-09-01 19:01:44 +0300

    seo: update manifest favicon

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -12,7 +12,7 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: "#050505",
     icons: [
       {
-        src: "/icon.svg",
+        src: "/noxa-favicon.svg",
         sizes: "any",
         type: "image/svg+xml",
         purpose: "any",
