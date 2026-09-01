9c6fe631f14433052c26b3a7fc8fdd24dcd5d0c5
poseidon <64107696+sergiosk222@users.noreply.github.com>
2026-09-01 18:55:38 +0300

    fix: use vector NOXA favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,14 +21,8 @@ export const metadata: Metadata = {
     },
   },
   icons: {
-    icon: [
-      { url: "/favicon.ico", sizes: "any" },
-      { url: "/icon.png", type: "image/png", sizes: "512x512" },
-    ],
-    shortcut: "/favicon.ico",
-    apple: [
-      { url: "/apple-icon.png", type: "image/png", sizes: "180x180" },
-    ],
+    icon: [{ url: "/icon.svg", type: "image/svg+xml" }],
+    shortcut: "/icon.svg",
   },
   robots: {
     index: true,
